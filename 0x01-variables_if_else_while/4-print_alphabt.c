7087b3c71febdcda3921a54be718896a06a41fa7
Shola Onoriemu <pyshola@gmail.com>
2022-10-14 18:50:53 +0100

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,18 +9,15 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 97; i < 123; i++)
 	{
-		if (i != 'q' || i != 'e')
+		if (i != 101 && i != 113)
 		{
-			putchar(alp[i]);
+			putchar(i);
 		}
-		
 	}
-	
 	putchar('\n');
 	return (0);
 }
