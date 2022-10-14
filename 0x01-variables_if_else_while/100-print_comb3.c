5cb619a135ac3bfb2fb87dfaa712f9ee9d02fdd2
Shola Onoriemu <pyshola@gmail.com>
2022-10-14 19:07:15 +0100

    print combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 89.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, s;
+
+	for(i = 48; i < 57; i++){
+		for(s = i + 1; s < 58; s++){
+			putchar(i);
+			putchar(s);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
