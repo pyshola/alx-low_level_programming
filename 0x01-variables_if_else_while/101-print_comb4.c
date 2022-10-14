9215cba969375ec1d46460349da561f83705f223
Shola Onoriemu <pyshola@gmail.com>
2022-10-14 19:14:17 +0100

    combination 2

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 012 to 789.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, s, j;
+
+	for (i = 48; i < 56; i++)
+	{
+		for (s = i + 1; s < 57; s++)
+		{
+			for (j = s + 1; s < 58; j++)
+			{
+				putchar(i);
+				putchar(s);
+				putchar(j);
+				if (i == 55 && s == 56 && j == 57)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
