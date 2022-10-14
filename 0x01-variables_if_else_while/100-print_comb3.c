18a64d49a3545bff5fe5bd4ec465d7639315e9f5
Shola Onoriemu <pyshola@gmail.com>
2022-10-14 19:09:39 +0100

    print combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,10 +9,16 @@ int main(void)
 {
 	int i, s;
 
-	for(i = 48; i < 57; i++){
-		for(s = i + 1; s < 58; s++){
+	for(i = 48; i < 57; i++)
+	{
+		for(s = i + 1; s < 58; s++)
+		{
 			putchar(i);
 			putchar(s);
+			if(i == 56 && s == 57)
+			{
+				break;
+			}
 			putchar(',');
 			putchar(' ');
 		}
