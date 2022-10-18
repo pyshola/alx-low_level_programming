bf208d4a036559d502fe582e5ae52b665cf6b127
Shola Onoriemu <pyshola@gmail.com>
2022-10-18 18:20:11 +0100

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isalpha - function that prints the sign of a number.
+ * print_sign - function that prints the sign of a number.
  *
- * @c: The character in ASCII code
+ * @n: The character in ASCII code
  *
  * Return: 1 for positive number and print +
  * retun zero for zero and return -1 for
@@ -16,7 +16,7 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
