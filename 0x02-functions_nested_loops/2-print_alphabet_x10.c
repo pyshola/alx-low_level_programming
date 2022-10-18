2b934382b964b175cddf27555b49e34675d4d433
Shola Onoriemu <pyshola@gmail.com>
2022-10-18 17:37:41 +0100

    print alphabet 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet 10X - Prints the alphabet in lowercase.
+ * print_alphabet 10X - Prints the alphabet 10 times .
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
-	int i, s;
-    
-    for (s = 0; s < 10; s++)
-    {
-        for (i = 97; i < 123; i++)
-        {
-            _putchar(i);
-        }
-    _putchar('\n');
-    }
+	int s, i;
+
+	for (s = 0; s < 10; s++)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+	_putchar('\n');
+	}
 }
