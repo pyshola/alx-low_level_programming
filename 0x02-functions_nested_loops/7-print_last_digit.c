bd0d685a7a957744fe97adf5bf2c1f554dbc5dda
Shola Onoriemu <pyshola@gmail.com>
2022-10-18 18:40:15 +0100

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,16 +13,14 @@ int print_last_digit(int n)
 	int s;
 	
 	s = n % 10;
-	if(s < 0)
+	if (s < 0)
 	{
-		_putchar((-1 * s ) + 48);
+		_putchar( (-1 * s ) + 48);
 		return(-1 * s);
-		
 	}
 	else
 	{
 		_putchar(s + 48);
 		return(s);
 	}
-	
 }
