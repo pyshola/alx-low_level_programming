cd1f9299a54dd4d9eef4f1b8eb34b0fe0ab07767
Shola Onoriemu <pyshola@gmail.com>
2022-10-18 15:11:39 +0100

    betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,8 +12,8 @@ int main(void)
 	int arr[] = {95, 112, 117, 116, 99, 104, 97, 114};
 
     int count, asize;
-    
     asize = sizeof(arr) / sizeof(int);
+    
     for (count = 0; count < asize; count++)
 	{
 		_putchar(arr[count]);
