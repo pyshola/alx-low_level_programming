e6898f120e146d3101acd3639fedb58fd6835334
Shola Onoriemu <pyshola@gmail.com>
2022-10-18 13:13:28 +0100

    betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * main - Program that prints _putchar, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char arr[] = "_putchar";
+    
+	char arr[] = "_putchar\n";
     int count, arr_size;
     arr_size = sizeof(arr) / sizeof(char);
+
 	for (count = 0; count < arr_size; count++)
 	{
 		_putchar(arr[count]);
