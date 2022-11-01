3498ced1d9b26fc3931598c9d4b927d0c8d338f8
Shola Onoriemu <pyshola@gmail.com>
2022-11-01 06:00:12 +0100

    more pointers

---
+++ 0x07-pointers_arrays_strings/main.h.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+
+#endif
