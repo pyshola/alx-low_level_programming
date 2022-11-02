68d58080eb0e54577dd6d12857e0f9ef5abc20dd
Shola Onoriemu <pyshola@gmail.com>
2022-11-02 16:54:56 +0100

    recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef __MAIN__H
+#define __MAIN__H
+
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+
+#endif
